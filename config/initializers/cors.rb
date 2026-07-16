f93cbe910bf4648a42ca48d499360bd1fae6dce9
Alexei EM <alexei377.2017@gmail.com>
2026-07-16 14:08:36 -0600

    Deploy on Render

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "https://pixelvault-front.onrender.com", "http://localhost:5173"
+
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
